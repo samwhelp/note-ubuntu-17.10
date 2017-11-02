c4b66ada6296a33e34e3139c8d27d5f0f894968a
samwhelp <samwhelp@users.noreply.github.com>
2017-11-02 14:14:41 +0800

    gedit candidate-encodings

---
+++ recipe/gedit/encodings/get.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+gsettings get org.gnome.gedit.preferences.encodings candidate-encodings
---
+++ recipe/gedit/encodings/set.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+gsettings set org.gnome.gedit.preferences.encodings candidate-encodings "['UTF-8', 'BIG5', 'BIG5-HKSCS', 'EUC-TW', 'GB18030', 'GB2312', 'GBK', 'CURRENT', 'ISO-8859-15', 'UTF-16']"

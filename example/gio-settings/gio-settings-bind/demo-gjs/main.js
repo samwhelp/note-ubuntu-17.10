b104dcd60bee844fcaaef31e2ce874b406e67507
samwhelp <samwhelp@users.noreply.github.com>
2017-11-12 18:54:35 +0800

    remove comment

--- example/gio-settings/gio-settings-bind/demo-gjs/main.js
+++ example/gio-settings/gio-settings-bind/demo-gjs/main.js
@@ -10,16 +10,17 @@ const NIGHT_LIGHT_TEMPERATURE_KEY = 'night-light-temperature';
 var Win = new Lang.Class({
 	Name: 'Win',
 	_init: function(args) {
+
 		this.win = new Gtk.Window({
 			title: 'Demo bind',
 			//default_width: 100,
 			//default_height: 60,
 			type: Gtk.WindowType.TOPLEVEL
 		});
-		this.win.set_position(Gtk.WindowPosition.CENTER) //http://devdocs.baznga.org/gtk30~3.22.12/gtk.windowposition#default-center
+		this.win.set_position(Gtk.WindowPosition.CENTER);
 		this.win.connect('delete-event', Gtk.main_quit);
 
-		//http://devdocs.baznga.org/gtk30~3.22.12/gtk.spinbutton
+
 		this.spin = Gtk.SpinButton.new_with_range(
 			1000,
 			7000,
@@ -27,14 +28,13 @@ var Win = new Lang.Class({
 		);
 		this.win.add(this.spin);
 
+
 		this.settings = new Gio.Settings({schema_id: GSD_PLUGINS_COLOR_SCHEMA});
-		//http://devdocs.baznga.org/gio20~2.50p/gio.settings#method-bind
-		//bind(key, object, property, flags)
 		this.settings.bind(
 			NIGHT_LIGHT_TEMPERATURE_KEY,
 			this.spin,
 			'value',
-			Gio.SettingsBindFlags.DEFAULT //http://devdocs.baznga.org/gio20~2.50p/gio.settingsbindflags
+			Gio.SettingsBindFlags.DEFAULT
 		);
 
 
